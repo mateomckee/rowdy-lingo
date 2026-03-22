bf4b2954256e09c00a1b880ba26d4380ba365485
Joshua Medel <joshua.medel@my.utsa.edu>
2026-03-21 21:16:38 -0500

    Added the Login.java and Main.java

---
+++ src/main/java/edu/utsa/cs3443/rowdylingo/Login.java
@@ -0,0 +1,32 @@
+package edu.utsa.cs3443.rowdylingo;
+import java.io.*;
+import java.util.*;
+
+public class Login {
+
+    public static int AttemptLogin(String username, String userPassword, String usersFile) {
+
+        int id = 0;
+
+        try {
+            BufferedReader bReader = new BufferedReader(new FileReader(usersFile));
+            String line = "";
+
+            while ((line = bReader.readLine()) != null) {
+                String[] date = line.split(",");
+                String usernameAttempt = data[1].trim();
+                String userPasswordAttempt = data[3].trim();
+
+                if (username.equals(usernameAttempt) && userPassword.equals(userPasswordAttempt)) {
+                    id = Integer.parseInt(data[0].trim());
+                }
+            }
+            bReader.close();
+        }
+        catch (Exception e) {
+            System.out.println("ERROR: File was not Found");
+        }
+
+        return id;
+    }
+}
